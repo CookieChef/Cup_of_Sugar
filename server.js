ab465a7a73ad90af271d4ceea60fd8635082df87
Sara Martinez <s_martinez_1990@outlook.com>
2019-11-13 22:17:12 -0500

    .

--- server.js
+++ server.js
@@ -2,12 +2,13 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const passport = require('passport');
+
 const users = require('./routes/api/users');
 
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 
