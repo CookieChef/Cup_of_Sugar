e84ba91e6a3e78287df643543b3f749d4c0d2ff0
Sara Martinez <s_martinez_1990@outlook.com>
2019-11-16 01:49:53 -0500

    working on getting calendar working

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser');
 const passport = require('passport');
 
 const users = require('./routes/api/users');
+const events = require('./routes/api/event');
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -22,6 +23,7 @@ app.use(passport.initialize());
 require('./config/passport')(passport);
 
 app.use('/api/users', users);
+app.use('/api/event', events);
 
 
 app.listen(port, () => {
